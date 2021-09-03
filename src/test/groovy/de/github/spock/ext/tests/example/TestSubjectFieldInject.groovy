ef1fb9442228d0c0cbbce1c823b1a430679b2367
Massimiliano Siddi <msid@d-velop.de>
2021-09-03 20:02:56 +0200

    Added some more annotation support; support for field, setter and constructor injection of beans.

--- src/test/groovy/de/github/spock/ext/tests/example/TestSubjectFieldInject.groovy
+++ src/test/groovy/de/github/spock/ext/tests/example/TestSubjectFieldInject.groovy
@@ -1,13 +1,10 @@
 package de.github.spock.ext.tests.example
 
-import org.springframework.beans.factory.annotation.Qualifier
-
 import javax.inject.Inject
 
 class TestSubjectFieldInject{
 
     @Inject
-    @Qualifier('richard')
     private TestInterface testInterface
 
     boolean doSomething(){
